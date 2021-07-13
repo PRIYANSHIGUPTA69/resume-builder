2d79a9638910d95fa6ee724a47327a3505f3585a
#priyu@123 <priyaengg26@gmail.com>
2021-07-13 23:04:11 +0700

    added upto routes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
-import React from 'react';
-import Ball from './Component/Ball';
 import Bat from './Component/Bat';
 import { Provider } from "react-redux";
 import store from './store';
 import User from './Component/User';
+import Ball from './Component/Ball';
+import Cart from './Component/Cart';
+import Home from './Component/Home';
+import Product from './Component/Product'
+import {BrowserRouter , Switch , Route} from "react-router-dom"
 function App() {
   return (
+
     // 4
-    <Provider store={store}>
-      <div className="App">
-        <Ball></Ball>
-        <Bat></Bat>
-        <User></User>
-      </div>
-    </Provider>
+     // redux example
+    // <Provider store={store}>
+    //   <div className="App">
+    //     <Ball></Ball>
+    //     <Bat></Bat>
+    //     <User></User>
+    //   </div>
+    // </Provider>
+   <> 
+  <BrowserRouter>
+  <Switch>
+    <Route path="/cart" component={Cart}></Route>
+    <Route path="/product" component={Product}></Route>
+    <Route path="/" component={Home}></Route>
+  </Switch>
+  </BrowserRouter>
+    </>
   );
 }
 export default App;
